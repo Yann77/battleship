5f435bfaa237a0461673430ef3d1b796969f24de
christopher Piette <christopher.piette@bnc.ca>
2019-08-29 09:35:51 -0400

    Oups

--- server/src/main/java/org/team/apps/web/WebSocketConfiguration.java
+++ server/src/main/java/org/team/apps/web/WebSocketConfiguration.java
@@ -1,8 +1,5 @@
 package org.team.apps.web;
 
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -11,24 +8,22 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.team.apps.board.BoardRepository;
 import org.team.apps.board.BoardService;
-import org.team.apps.game.GameController;
 import org.team.apps.game.GameRepository;
 import org.team.apps.game.GameService;
 import org.team.apps.user.UserRepository;
 
 @Configuration
 @EnableWebSocketMessageBroker
-@AllArgsConstructor
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
+    public WebSocketConfiguration() {
+    }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
-
-
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
